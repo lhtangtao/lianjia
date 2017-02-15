b8227334c77a3b6ccb3b81820a3e68e510aed931
tangtao <rache008@163.com>
2017-02-15 17:25:25 +0800

    目前能从网上爬下所有的房子信息和单价总价，但是需要对房子的信息进行字符串处理

---
+++ test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#name   = test
+#author = tangtao
+#time   = 2017/2/15 16:45
+#Description=添加描述信息
+#eMail   =tangtao@lhtangtao.com
+#git     =lhtangtao
+# code is far away from bugs with the god animal protecting
+I love animals. They taste delicious.
+┏┓      ┏┓
+┏┛┻━━━┛┻┓
+┃      ☃      ┃
+┃  ┳┛  ┗┳  ┃
+┃      ┻      ┃
+┗━┓      ┏━┛
+┃      ┗━━━┓
+┃  神兽保佑    ┣┓
+┃　永无BUG！   ┏┛
+┗┓┓┏━┳┓┏┛
+┃┫┫  ┃┫┫
+┗┻┛  ┗┻┛
+"""
+import urllib2
+import re
+import sys
+import urllib2
+import time
+from bs4 import BeautifulSoup
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+time.clock()
+i = 1
+while i < 2:
+    url = 'http://hz.lianjia.com/ershoufang/binjiang/pg' + str(i) + '/'
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page, "lxml")
+    for title in soup.find_all('a'):
+        total_price = title.get('href')
+        print(total_price)
+    # for link in soup.find_all('div', 'houseInfo'):
+    #     context = link.get_text()
+    #     print(str(i) + context)
+    # for price in soup.find_all('div', 'totalPrice'):
+    #     total_price = price.get_text()
+    #     print(total_price)
+    i += 1
+# print("end" + time.clock())
