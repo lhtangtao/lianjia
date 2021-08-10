436eacae0aa1dbeb2da886137a9e36b15f4bcef0
tao tang <rache008@163.com>
2021-08-10 14:06:57 +0800

    在实现区的下一级单位获取数据 并存放到指定的文件上

--- file_action.py
+++ file_action.py
@@ -9,14 +9,37 @@
 #my_website=http://www.lhtangtao.com
 #Description=存放文件操作的相关函数
 """
-from cities import get_sub_location
+from cities import get_sub_location, get_location
+import sys
 
-def write_file(path, text):
-    test = open(path, "w+")
-    test.write(text + '\n')
-    test.close()
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def write_sub_location(city="hz"):
+    """
+    把该城市的所有子区域读取出来
+    :param city:
+    :return:
+    """
+    file_address = "./city_file/" + city
+    open(file_address, "w+")
+    list_location = get_location("http://"+city+".lianjia.com/ershoufang/")
+    for i in range(len(list_location)):
+
+        url_to_add = list_location[i].split("=")[1]
+        print url_to_add
+        url = "https://" + city + ".lianjia.com" + url_to_add
+        print url
+        try:
+            for j in range(len(get_sub_location(url))):
+                x = get_sub_location(url)[j]
+                print x
+                test = open(file_address, "a+")
+                test.write(x + '\n')
+        except:
+            print list_location[i] + u"这个区没有数据"
 
 
 if __name__ == '__main__':
-    print get_sub_location()
-    write_file("./city_file/NB", "wwwwwww")
+    write_sub_location("nb")
